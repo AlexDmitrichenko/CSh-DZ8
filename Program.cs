1c3b926a3fd00daf8221e01273eaeddabfb962fe
Alex <best1972@list.ru>
2023-01-29 12:38:41 +0300

    made part 1 task 3

--- Program.cs
+++ Program.cs
@@ -124,10 +124,73 @@ void GetNumberRowMin(int[] array)
     }
     Console.Write($"Номер строки с наименьшей суммой элементов: {minrow}");
 }*/
-
-
-
-
+//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+//Например, даны 2 матрицы:
+//2 4 | 3 4
+//3 2 | 3 3
+//Результирующая матрица будет:
+//18 20
+//15 18
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
+int[,] array1 = GetArray1(rows, columns, 0, 10);
+PrintArray(array1);
+Console.WriteLine();
+int[,] array2 = GetArray2(rows, columns, 0, 10);
+PrintArray(array2);
+Console.WriteLine();
+int[,] ChangedArray = GetMultiplicationArrays(newArray);
+PrintArray(ChangedArray);
+//Methods---------------------------------------------------- 
+int[,] GetArray1(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+int[,] GetArray2(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] GetMultiplicationArrays(int[,] array1, int[,] array2)
+{
+    int[,] newArray = new int[array1.GetLength(0), array2.GetLength(1)];
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            newArray[i, j] = array[j, i];
+        }
+    }
+    return newArray;
+}
 
 
 
